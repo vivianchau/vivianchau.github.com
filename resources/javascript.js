ba104930d8a14d13e8328091569c3940d9ce9dc5
Vivian Chau <vivian@tradecrafted.com>
2014-01-10 16:43:06 -0800

    committing JS changes

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,29 @@
-// Your Javascript will go here!
+$(document).ready(function() {
+ function replaceName() {
+    var myNewName = window.prompt('Pick a new name!');
+    //document.getElementById('user-name').innerHTML = myNewName;
+    $("#user-name").html(myNewName);
+    $("#user-name").css({color:"blue"});
+  }
+  
+  function clickEvent() {
+    $('#copyright').click( function(){
+        $(this).css({'color': 'green', 'font-size':'20px'});
+    }); 
+  }    
+        $('#user-name').click(replaceName);
+        
+        $('.section').hover(
+            function(){ $(this).css({ 'background-color': 'black' }); },
+            function(){ $(this).css({ 'background-color': 'orange' }); }
+        );
+      
+  
+  $('#copyright').click(function(){
+    $(this).css({'color': 'green', 'font-size':'20px'});
+    });
+
+  replaceName();
+  clickEvent();
+    });
+
